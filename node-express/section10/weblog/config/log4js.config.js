1b814d4c8b3b9684f8fc00b213562fa2f1b6cba2
Dai FUJIHARA@daipresents <daipresents@gmail.com>
2020-04-14 00:03:19 +0900

    finish 1--59

--- node-express/section10/weblog/config/log4js.config.js
+++ node-express/section10/weblog/config/log4js.config.js
@@ -1,13 +1,26 @@
+var path = require("path");
+var ROOT = path.join(__dirname, "../");
+
 module.exports = {
-  appenders:{
+  appenders: {
     ConsoleLogAppender:{
       type: "console"
+    },
+    FileLogAppender: {
+      type: "file",
+      filename: path.join(ROOT, "./log/system/system.log"),
+      maxLogSize: 5000000,
+      backups: 10
     }
   },
-  categories:{
+  categories: {
     "default": {
       appenders: ["ConsoleLogAppender"],
       level: "ALL"
+    },
+   system: {
+      appenders: ["FileLogAppender"],
+      level: "ERROR"
     }
   }
 };
