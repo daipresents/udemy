e4a1a01fcc4c74e3abfb846d5b13794ee551b2e8
Dai FUJIHARA@daipresents <daipresents@gmail.com>
2020-04-15 23:46:58 +0900

    finish 10-69

--- node-express/section10/weblog/routes/account.js
+++ node-express/section10/weblog/routes/account.js
@@ -1,37 +1,41 @@
+var { CONNECTION_URL, OPTIONS, DATABSE } = require("../config/mongodb.config");
 var router = require("express").Router();
+var MongoClient = require("mongodb").MongoClient;
+
 var validateRegistData = function (body) {
   var isValidated = true, errors = {};
 
-  if (!body.url){
+  if (!body.url) {
     isValidated = false;
     errors.url = "URLが未入力です。'/'から始まるURLを入力してください。";
   }
 
-  if (body.url && /^\//.test(body.url) === false){
+  if (body.url && /^\//.test(body.url) === false) {
     isValidated = false;
     errors.url = "'/'から始まるURLを入力してください。";
   }
 
-  if (!body.title){
+  if (!body.title) {
     isValidated = false;
     errors.title = "タイトルが未入力です。任意のタイトルを入力してください。";
   }
 
   return isValidated ? undefined : errors;
-}
+};
 
 var createRegistData = function (body) {
   var datetime = new Date();
   return {
     url: body.url,
     published: datetime,
-    update: datetime,
+    updated: datetime,
     title: body.title,
     content: body.content,
     keywords: (body.keywords || "").split(","),
     authors: (body.authors || "").split(","),
-  }
-}
+  };
+};
+
 router.get("/", (req, res) => {
   res.render("./account/index.ejs");
 });
@@ -53,5 +57,28 @@ router.post("/posts/regist/confirm", (req, res) => {
     return;
   }
   res.render("./account/posts/regist-confirm.ejs", { original });
-})
+});
+
+router.post("/posts/regist/execute", (req, res) => {
+  var original = createRegistData(req.body);
+  var errors = validateRegistData(req.body);
+  if (errors) {
+    res.render("./account/posts/regist-form.ejs", { errors, original });
+    return;
+  }
+
+  MongoClient.connect(CONNECTION_URL, OPTIONS, (error, client) => {
+    var db = client.db(DATABSE);
+    db.collection("posts")
+      .insertOne(original)
+      .then(() => {
+        res.render("./account/posts/regist-complete.ejs");
+      }).catch((error) => {
+        throw error;
+      }).then(() => {
+        client.close();
+      });
+  });
+});
+
 module.exports = router;
