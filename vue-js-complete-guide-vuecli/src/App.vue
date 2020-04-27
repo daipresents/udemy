178c36dd271194d0d1b8d97d0db316bd3067e9be
Dai FUJIHARA@daipresents <daipresents@gmail.com>
2020-04-27 21:58:06 +0900

    finish 7-91

--- vue-js-complete-guide-vuecli/src/App.vue
+++ vue-js-complete-guide-vuecli/src/App.vue
@@ -3,7 +3,7 @@
     <LikeHeader></LikeHeader>
     <h2>{{ number }}</h2>
     <LikeNumber :totalNumber="number"></LikeNumber>
-    <LikeNumber :totalNumber="number"></LikeNumber>
+    <LikeNumber></LikeNumber>
   </div>
 </template>
 
@@ -13,7 +13,7 @@ import LikeHeader from "./components/LikeHeader";
 export default {
   data() {
     return {
-      number: 10
+      number: 12
     };
   },
   components: {
--- vue-js-complete-guide-vuecli/src/components/LikeNumber.vue
+++ vue-js-complete-guide-vuecli/src/components/LikeNumber.vue
@@ -7,7 +7,12 @@
 
 <script>
 export default {
-  props: ['totalNumber'],
+  props: {
+    totalNumber: {
+      type: Number,
+      default: 5
+    }
+  },
   computed: {
     halfNumber() {
       return this.totalNumber / 2;
